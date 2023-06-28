fff040d259875d06d50fb60c8e89f950ebd13972
DiegoUgaz <diegoroot2002@gmail.com>
2023-06-28 18:56:21 -0500

    Subiendo modulo del Asistente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegistroComponent } from './registro/registro.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { EpecialistaComponent } from './epecialista/epecialista.component';
+import { RutinasComponent } from './rutinas/rutinas.component';
+import { TiporutinaComponent } from './tiporutina/tiporutina.component';
+import { AsistenteComponent } from './asistente/asistente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegistroComponent,
+    CatalogoComponent,
+    PerfilComponent,
+    EpecialistaComponent,
+    RutinasComponent,
+    TiporutinaComponent,
+    AsistenteComponent
   ],
   imports: [
     BrowserModule,
