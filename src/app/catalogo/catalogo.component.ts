422bb9cac3b62d629cef2b0b47e994653e23753f
DiegoUgaz <diegoroot2002@gmail.com>
2023-07-12 10:41:22 -0500

    Subiendo Editar Perfil

---
+++ src/app/catalogo/catalogo.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-catalogo',
+  templateUrl: './catalogo.component.html',
+
+  styleUrls: ['./catalogo.component.css']
+})
+export class CatalogoComponent implements OnInit {
+  especialistas = [
+    { nombre: 'Jose Montolla', tipo: 'Especialista' },
+    { nombre: 'Maria Fernanda', tipo: 'Especialista' },
+    { nombre: 'Fatima Camus', tipo: 'Especialista' }
+  ];
+  asistentes = [
+    { nombre: 'Jean Paul', tipo: 'Asistente' },
+    { nombre: 'Allison Yesenia', tipo: 'Asistente' },
+    { nombre: 'Shwila Elina', tipo: 'Asistente' }
+  ];
+  resultadosEspecialistas = this.especialistas;
+  resultadosAsistentes = this.asistentes;
+
+  buscar(texto: string): void {
+    this.resultadosEspecialistas = this.especialistas.filter((especialista) =>
+      especialista.nombre.toLowerCase().includes(texto.toLowerCase())
+    );
+    this.resultadosAsistentes = this.asistentes.filter((asistente) =>
+      asistente.nombre.toLowerCase().includes(texto.toLowerCase())
+    );
+  }
+
+  ngOnInit(): void {}
+}
